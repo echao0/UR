7f1b2b7ddbb2af37538d08f35849d6b1a768da5e
echao0 <echao@Mac-echao-86.local>
2018-12-19 19:14:59 +0100

    seguridad OK

--- NodeMCU/UrDevice/UrDevice.ino
+++ NodeMCU/UrDevice/UrDevice.ino
@@ -7,8 +7,9 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 
+
 #ifndef STASSID
-#define STASSID "Tersawifi"
+#define STASSID "Teresawifi"
 #define STAPSK  "locastela"
 #endif
 
@@ -22,8 +23,36 @@ const uint16_t port = 8000;
 
 ESP8266WiFiMulti WiFiMulti;
 
+//----------DEF I/O-------
+  int tempPin= A0;
+  char out1 = D1;
+  char out2 = D2;
+  char out3 = D3;
+  char out4 = D4;
+  char out5 = D5;
+  
+  char alto = LOW;
+  char bajo = HIGH;
+    
+//-----------------------------
+
+
 void setup() {
   Serial.begin(115200);
+  
+//-----------IN/OUT------------
+  pinMode(out1, OUTPUT);
+  digitalWrite(out1, bajo);
+  pinMode(out2, OUTPUT);
+  digitalWrite(out2, bajo);
+  pinMode(out3, OUTPUT);
+  digitalWrite(out3, bajo);
+  pinMode(out4, OUTPUT);
+  digitalWrite(out4, bajo);
+  pinMode(out5, OUTPUT);
+  digitalWrite(out5, bajo);
+  pinMode(0, INPUT); // Boton de flash
+//-----------------------------
 
   // We start by connecting to a WiFi network
   WiFi.mode(WIFI_STA);
@@ -60,16 +89,25 @@ void loop() {
         delay(5000);
         return;
     }
-//-----------------PASSWORD APP-------------
+//-----------------Send NAME-------------
     if (client.connected()) {
-        client.println(user_passw);
+       client.println(user_passw);
+        delay(500);
+        client.println("name,urControl");
       }
  //----------------------------------------
  
     while (client.connected()) {
+         
+        if (digitalRead(0) == 0){
+          client.println("TestNodeMcu");
+          Serial.println("Test send");
+          }
+         
         Serial.println("receiving from remote server");
         String line = client.readStringUntil('\r');
-    
+        Serial.println(line);
+            
           if (line == "t"){
                 client.println("ack");
             }
@@ -77,11 +115,13 @@ void loop() {
            if (line == "DeviceOn"){
                 Serial.println ("Enciendo todo");
                 client.println("Enciendo todo");
+                digitalWrite(out1, alto);
             }
             
            if (line == "DeviceOff"){
                 Serial.println ("Apago todo");
                 client.println("Apago todo");
+                digitalWrite(out1, bajo);
               }
           
         delay(100);
--- Server/UrServer.py
+++ Server/UrServer.py
@@ -13,18 +13,53 @@ libc = ctypes.cdll.LoadLibrary('libc.so.6')
 libc.prctl(15, 'UrServer', 0, 0, 0)
 # -----------------------------------------------------------------------
 
+NodePassword = 8613
 sleep_time = 0.7  # Time to wait in infinite loop (CPU control)
 
 sel = selectors.DefaultSelector()
 
+def packetHandling(data,key):
+
+        data = data.decode('UTF-8')  # convert to string (Python 3 only)
+        data = data.replace("\n", "")  # remove newline character
+        data = data.replace("\r", "")  # remove newline character
+
+
+        if data.count(",") == 1:
+            data = data.split(",")
+            print(data[0])
+            print(data[1])
+            if data[0] == "name":
+                key.name = data[1]
+
+        else:
+            print(data)
+
+        if data == "TestNodeMcu":
+            return b'ack_server'
+
+
 
 def accept_wrapper(sock):
     conn, addr = sock.accept()  # Should be ready to read
-    print("accepted connection from", addr)
-    conn.setblocking(False)
-    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"", name=b"test_name1")
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn, events, data=data)
+    recv_data = conn.recv(1024)  # Should be ready to read
+    if recv_data:
+        data = recv_data
+        data = data.decode('UTF-8')  # convert to string (Python 3 only)
+        data = data.replace("\n", "")  # remove newline character
+        data = data.replace("\r", "")  # remove newline character
+        data = int(data)
+        print(data)
+
+        if (data == NodePassword):
+            print("accepted connection from", addr)
+            conn.setblocking(False)
+            data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"", name=b"test_name1")
+            events = selectors.EVENT_READ | selectors.EVENT_WRITE
+            sel.register(conn, events, data=data)
+        else:
+            print("Password incorrecto", addr)
+            conn.close()
 
 
 def service_connection(key, mask):
@@ -33,7 +68,8 @@ def service_connection(key, mask):
     if mask & selectors.EVENT_READ:
         recv_data = sock.recv(1024)  # Should be ready to read
         if recv_data:
-            data.outb += recv_data
+            #data.outb += recv_data
+            data.outb = packetHandling(recv_data,data)
         else:
             print("closing connection to", data.addr)
             sel.unregister(sock)
@@ -62,7 +98,7 @@ try:
     while True:
         events = sel.select(timeout=None)
         for key, mask in events:
-            print (key.data)
+            #print (key.data)
             if key.data is None:
                 accept_wrapper(key.fileobj)
             else:
